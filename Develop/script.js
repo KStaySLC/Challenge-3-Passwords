38e370843e6c71477b2e64b8f239eb9836116e5f
Kevin Stayner <kstayslc@icloud.com>
2022-05-06 09:10:00 -0600

    Completed .css styling

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
 // Assignment code here
-
+var numberOfCharacter = document.getElementById("numberOfCharacters")
+var 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
